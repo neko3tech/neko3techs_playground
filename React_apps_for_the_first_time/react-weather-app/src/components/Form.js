7b9a2dec43532f3de3dfca6c5da47fccf8616287
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-04-29 21:23:43 +0900

    Form.jsを作ろう その2

--- React_apps_for_the_first_time/react-weather-app/src/components/Form.js
+++ React_apps_for_the_first_time/react-weather-app/src/components/Form.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import axios from 'axios';
 
 const Form = () => {
+    const api_key = process.env.REACT_APP_WEATHERAPI_KEY;
     const [city, setCity] = useState("");
+    const getWeather = (e) => {
+        e.preventDefault();
+        axios.get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=London&aqi=no`)
+            .then(
+                res => console.log(res)
+            );
+    };
+
     return (
         <form>
             <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-            {city}
-            <button type="submit">Get Weather</button>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 };
