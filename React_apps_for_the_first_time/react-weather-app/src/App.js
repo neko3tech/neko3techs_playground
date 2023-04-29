f4288f062a0d03677e35fb6d3f92a66ded5de9e0
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-04-29 21:46:14 +0900

    Results.jsを作ろう その1

--- React_apps_for_the_first_time/react-weather-app/src/App.js
+++ React_apps_for_the_first_time/react-weather-app/src/App.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
+import axios from 'axios';
+
 import Title from "./components/Title"
 import Form from "./components/Form"
+import Results from "./components/Results";
+
 import './App.css';
 
 function App() {
+
+  const api_key = process.env.REACT_APP_WEATHERAPI_KEY;
+  const [city, setCity] = useState("");
+  const getWeather = (e) => {
+    e.preventDefault();
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=London&aqi=no`)
+      .then(
+        res => console.log(res)
+      );
+  };
+
   return (
     <div className="hello">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results />
     </div>
   );
 }
--- React_apps_for_the_first_time/react-weather-app/src/components/Form.js
+++ React_apps_for_the_first_time/react-weather-app/src/components/Form.js
@@ -1,21 +1,8 @@
-import { useState } from "react";
-import axios from 'axios';
-
-const Form = () => {
-    const api_key = process.env.REACT_APP_WEATHERAPI_KEY;
-    const [city, setCity] = useState("");
-    const getWeather = (e) => {
-        e.preventDefault();
-        axios.get(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=London&aqi=no`)
-            .then(
-                res => console.log(res)
-            );
-    };
-
+const Form = (props) => {
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} />
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     );
 };
