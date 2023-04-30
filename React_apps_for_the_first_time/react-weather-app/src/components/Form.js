a8d31a0dc318570f33c63e3b49da856206144416
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-04-30 11:18:06 +0900

    第4章 フォームの入力文字を消す

--- React_apps_for_the_first_time/react-weather-app/src/components/Form.js
+++ React_apps_for_the_first_time/react-weather-app/src/components/Form.js
@@ -1,7 +1,7 @@
-const Form = ({ setCity, getWeather }) => {
+const Form = ({ setCity, getWeather, city }) => {
     return (
         <form onSubmit={getWeather}>
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
+            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} value={city} />
             <button type="submit">Get Weather</button>
         </form>
     );
