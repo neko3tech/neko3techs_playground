775423965d2936250cb51dc48ce6f7bf972e39a3
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-04-30 11:13:13 +0900

    第4章 <>の利用

--- React_apps_for_the_first_time/react-weather-app/src/components/Results.js
+++ React_apps_for_the_first_time/react-weather-app/src/components/Results.js
@@ -2,14 +2,14 @@ const Results = ({ results }) => {
     const { country, cityName, temperature, conditionText, icon } = results;
 
     return (
-        <div>
+        <>
             {country && <div className="results-country">{country}</div>}
             {cityName && <div className="results-city">{cityName}</div>}
             {temperature && <div className="results-temp">{temperature}<span>℃</span></div>}
             {conditionText && <div className="results-condition">
                 <img src={icon} alt="icon" />
                 <span>{conditionText}</span></div>}
-        </div>
+        </>
     );
 };
 
