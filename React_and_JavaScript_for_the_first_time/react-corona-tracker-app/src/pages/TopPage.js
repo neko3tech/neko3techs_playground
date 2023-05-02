d89c351f3c2a3d7ea0e786375e03d8095e0200de
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-02 19:10:31 +0900

    Selector.js を作ろう その1

--- React_and_JavaScript_for_the_first_time/react-corona-tracker-app/src/pages/TopPage.js
+++ React_and_JavaScript_for_the_first_time/react-corona-tracker-app/src/pages/TopPage.js
@@ -1,10 +1,12 @@
 import Title from '../components/Title';
+import Selector from '../components/Selector';
 
 
 const TopPage = () => {
     return (
         <div>
             <Title />
+            <Selector />
         </div>
     );
 };
