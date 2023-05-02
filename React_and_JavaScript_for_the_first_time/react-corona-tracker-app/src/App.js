a9e0f055adca9fcd321f5db7b5bef75a1b17ffd5
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-02 17:50:39 +0900

    Title.jsを作ろう

--- React_and_JavaScript_for_the_first_time/react-corona-tracker-app/src/App.js
+++ React_and_JavaScript_for_the_first_time/react-corona-tracker-app/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import TopPage from './pages/TopPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <TopPage />
     </div>
   );
 }
---
+++ React_and_JavaScript_for_the_first_time/react-corona-tracker-app/src/pages/TopPage.js
@@ -0,0 +1,12 @@
+import Title from '../components/Title';
+
+
+const TopPage = () => {
+    return (
+        <div>
+            <Title />
+        </div>
+    );
+};
+
+export default TopPage;
