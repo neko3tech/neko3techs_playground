c942556bc36f5a1871f5689824fd914997382fe2
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-03 19:09:49 +0900

    Results.jsを作ろう その2

--- React_and_JavaScript_for_the_first_time/react-corona-tracker-app/src/App.js
+++ React_and_JavaScript_for_the_first_time/react-corona-tracker-app/src/App.js
@@ -7,11 +7,26 @@ import countriesJson from './countries.json';
 function App() {
 
   const [country, setCountry] = useState("");
+  const [countryData, setCountryData] = useState({
+    date: "",
+    newConfirmed: "",
+    totalConfirmed: "",
+    newRecoverd: "",
+    totalRecoverd: ""
+  });
 
   const getCountryData = () => {
     fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        setCountryData({
+          date: data[data.length - 1].Date,
+          newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
+          totalConfirmed: data[data.length - 1].Confirmed,
+          newRecoverd: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
+          totalRecoverd: data[data.length - 1].Recovered
+        });
+      })
   };
 
   return (
