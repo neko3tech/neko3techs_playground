5fe5df995585b500049944d72448359ba06a0e7d
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-07 17:41:57 +0900

    アクセス数の制限機能

--- How_to_make_a_proxy_server/index.js
+++ How_to_make_a_proxy_server/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
-const { createProxyMiddleware } = require("http-proxy-middleware")
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
+
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 1000,
+});
 
 app.get("/", (req, res) => {
     res.send("This is my proxy servers.");
 });
 
-app.use("/corona-tracker-world-data", (req, res, next) => {
+app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
     createProxyMiddleware({
         target: "https://monotein-books.vercel.app/api/corona-tracker/summary",
         changeOrigin: true,
