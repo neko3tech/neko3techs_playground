10ad4d22163cda6b73086006df2415b017ca5404
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-09 17:42:57 +0900

    プロキシサーバーの使い方 過去2冊分向けのパス作成

--- How_to_make_a_proxy_server/index.js
+++ How_to_make_a_proxy_server/index.js
@@ -5,18 +5,45 @@ const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 const url = require("url");
 
-
+// Setting rateLimit.
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 1000,
 });
 
+
+// root
 app.get("/", (req, res) => {
     const params = url.parse(req.url).query;
     console.log(params);
-    res.send("This is my proxy servers.");
+    res.send("This is my proxy server.");
 });
 
+// weather-data
+app.use("/weather-data", limiter, (req, res, next) => {
+    const city = url.parse(req.url).query;
+    createProxyMiddleware({
+        target: `${process.env.BASE_API_URL_WEATHERAPI}${city}`,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/weather-data`]: "",
+        },
+    })(req, res, next)
+});
+
+// corona-tracker-country-data
+app.use("/corona-tracker-country-data", limiter, (req, res, next) => {
+    const city = url.parse(req.url).query;
+    createProxyMiddleware({
+        target: `${process.env.BASE_API_URL_CORONA_COUNTRY}/${city}`,
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/corona-tracker-country-data`]: "",
+        },
+    })(req, res, next)
+});
+
+// corona-tracker-world-data
 app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
     createProxyMiddleware({
         target: `${process.env.BASE_API_URL_CORONA_WORLD}`,
@@ -27,8 +54,9 @@ app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
     })(req, res, next)
 });
 
-const port = process.env.PORT || 5000;
 
+// Listening start.
+const port = process.env.PORT || 5000;
 app.listen(5000, () => {
     console.log(`Listening on port:${port}`);
 });
