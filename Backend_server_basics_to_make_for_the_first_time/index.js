c564b28b3e0e50ac0beec1c1aef5fd45f6b91cd4
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-12 21:01:43 +0900

    GETリクエスト（文字を返す）

--- Backend_server_basics_to_make_for_the_first_time/index.js
+++ Backend_server_basics_to_make_for_the_first_time/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("<h1>見出し1</h1>");
+});
 
 app.listen(5000, () => {
     console.log("Listening on localhost:5000");
