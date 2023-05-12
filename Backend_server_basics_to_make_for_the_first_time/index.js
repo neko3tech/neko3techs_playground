afda43c367ba9f602e0fd1b29b6d535ce6d36433
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-12 21:42:56 +0900

    POSTリクエスト

--- Backend_server_basics_to_make_for_the_first_time/index.js
+++ Backend_server_basics_to_make_for_the_first_time/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.sendfile(__dirname + "/index.html");
+});
+
+app.post("/autumn", (req, res) => {
+    console.log("req.body : ", req.body);
 });
 
 app.listen(5000, () => {
