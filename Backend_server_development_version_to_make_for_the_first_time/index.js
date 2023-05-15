a4d0d12ee801c0bff81124e9d5cbe2191b136301
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-16 07:27:57 +0900

    ブログのデータ作成機能

--- Backend_server_development_version_to_make_for_the_first_time/index.js
+++ Backend_server_development_version_to_make_for_the_first_time/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
+const rootDir = __dirname;
+const views = `${rootDir}/views`;
 
-// root
+// expressのURLエンコードを有効にする
+app.use(express.urlencoded({ extended: true }));
+
+
+// get:root
 app.get("/", (req, res) => {
     res.send("こんにちは")
 });
 
+// get:blog/create
+app.get("/blog/create", (req, res) => {
+    res.sendFile(`${views}/blogCreate.html`);
+});
+
+// post:blog/create
+app.post("/blog/create", (req, res) => {
+    console.log(req.body)
+    res.send("こんにちは");
+});
 
 // Listening start.
 app.listen(5000, () => {
