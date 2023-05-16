53b100d3f12bcd8c143d3073e43fe7f62e758fdb
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-05-16 18:40:42 +0900

    MongoDBにデータを書き込む

--- Backend_server_development_version_to_make_for_the_first_time/index.js
+++ Backend_server_development_version_to_make_for_the_first_time/index.js
@@ -1,26 +1,62 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
 const app = express();
 const rootDir = __dirname;
 const views = `${rootDir}/views`;
+const { MONGO_DB_CON_STR } = process.env;
+
 
 // expressのURLエンコードを有効にする
 app.use(express.urlencoded({ extended: true }));
 
 
+// connect mongodb
+mongoose.connect(MONGO_DB_CON_STR)
+    .then(() => {
+        console.log("Success: Connected to MongoDB.");
+    })
+    .catch((err) => {
+        console.err("Failuer: Unconnected to MongoDB.")
+    });
+
+// db schema
+const Schema = mongoose.Schema;
+const BlogSchema = new Schema({
+    title: String,
+    summary: String,
+    image: String,
+    textBody: String,
+});
+const BlogModel = mongoose.model("Blog", BlogSchema);
+
+
 // get:root
 app.get("/", (req, res) => {
-    res.send("こんにちは")
+    res.send("こんにちは");
 });
+
 
 // get:blog/create
 app.get("/blog/create", (req, res) => {
     res.sendFile(`${views}/blogCreate.html`);
 });
 
-// post:blog/create
+
+// post:/blog/create
 app.post("/blog/create", (req, res) => {
-    console.log(req.body)
-    res.send("こんにちは");
+    BlogModel.create(req.body)
+        .then((data) => {
+            console.log("データの書き込みが成功しました。", data);
+            res.send("ブログデータの投稿が成功しました。");
+        })
+        .catch((error) => {
+            console.error("データの書き込みが失敗しました。", error);
+            res.send("ブログデータの投稿が失敗しました。");
+        });
+
 });
 
 // Listening start.
