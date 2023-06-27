fa29a6d2a825f99360d49ba1e8044899d15262bb
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-06-27 14:36:59 +0900

    article.jsの作成

--- Create_your_first_Gatsby_site/gatsby-portfolio/src/templates/article.js
+++ Create_your_first_Gatsby_site/gatsby-portfolio/src/templates/article.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 const article = ({ data }) => {
     return (
         <>
-            <h1>{data.markdownRemark.frontmatter.title}</h1>
-            <p>{data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+            <div>
+                <GatsbyImage image={data.markdownRemark.frontmatter.image.childImageSharp.gatsbyImageData} alt="article-image" />
+            </div>
+            <div>
+                <h1>{data.markdownRemark.frontmatter.title}</h1>
+                <p>{data.markdownRemark.frontmatter.date}</p>
+                <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+            </div>
         </>
     )
 }
@@ -21,6 +27,11 @@ export const query = graphql`
                 excerpt
                 id
                 title
+                image {
+                    childImageSharp {
+                        gatsbyImageData(quality: 90, formats: [AUTO, WEBP, AVIF], placeholder: BLURRED, width: 1000)
+                    }
+                }
             }
             html
         }
