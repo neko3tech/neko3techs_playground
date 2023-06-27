67d453a38c44ca9380a84a1068fea06f9c27ff17
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-06-27 14:59:30 +0900

    contact.jsの作成

---
+++ Create_your_first_Gatsby_site/gatsby-portfolio/src/pages/contact.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+const Contact = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>Contact</h1>
+                    <p>お気軽にご連絡ください</p>
+                    <form>
+                        <label htmlFor="name">おなまえ</label>
+                        <input type="text" name="name" id="name" required />
+                        <label htmlFor="email">メールアドレス</label>
+                        <input type="email" name="email" id="email" required />
+                        <label htmlFor="message">メッセージ</label>
+                        <textarea name="message" id="message" rows="10" required ></textarea>
+                        <button type="submit">送信</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Contact;
