b396b5c218ded880dcc988b2ce1b5116470e2938
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-08-09 21:10:15 +0900

    ユーザー作成

--- Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/pages/api/user/create.js
+++ Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/pages/api/user/create.js
@@ -1,19 +1,19 @@
 import connectDB from "@/utils/database";
-import { ItemModel } from "@/utils/schemaModels";
+import { UserModel } from "@/utils/schemaModels";
 
 export default async (req, res) => {
   try {
     await connectDB();
-    const data = await ItemModel.create(req.body);
+    const data = await UserModel.create(req.body);
 
     return res.status(200).json({
-      message: `アイテム作成成功`,
+      message: `ユーザー作成成功`,
       result: data,
     })
 
   } catch (error) {
     return res.status(400).json({
-      message: `アイテム作成失敗`,
+      message: `ユーザー作成失敗`,
       error: error
     })
   }
--- Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/utils/schemaModels.js
+++ Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/utils/schemaModels.js
@@ -8,4 +8,21 @@ const ItemSchema = new Schema({
   email: String,
 });
 
-export const ItemModel = mongoose.model("Item", ItemSchema);
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const ItemModel = mongoose.models.Item || mongoose.model("Item", ItemSchema);
+export const UserModel = mongoose.models.User || mongoose.model("User", UserSchema);
