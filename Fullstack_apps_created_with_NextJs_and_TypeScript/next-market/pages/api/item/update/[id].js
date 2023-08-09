0bb471bae71faa73678dc58b341e0b97a0cec430
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-08-09 15:44:59 +0900

    アイテム更新

---
+++ Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/pages/api/item/update/[id].js
@@ -0,0 +1,22 @@
+import connectDB from "@/utils/database";
+import { ItemModel } from "@/utils/schemaModels";
+
+export default async (req, res) => {
+  try {
+    console.log('req.query: ', req.query);
+    console.log('req.body: ', req.body);
+    await connectDB();
+    await ItemModel.updateOne({ _id: req.query.id }, req.body);
+
+    return res.status(200).json({
+      message: `アイテム更新成功`,
+    })
+
+  } catch (error) {
+    return res.status(400).json({
+      message: `アイテム更新失敗`,
+      error: error,
+    })
+  }
+
+};
