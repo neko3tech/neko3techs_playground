38c59ffc9dba6c0a95859e1f3f3a1035f8c05ef8
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-08-31 12:14:18 +0900

    userのTypeScript移行

--- Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/pages/user/login.tsx
+++ Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/pages/user/login.tsx
@@ -1,8 +1,8 @@
+import type { NextPage } from "next";
 import { useState } from "react";
 import Head from "next/head";
 
-export default () => {
-
+const Login: NextPage = () => {
   const [user, setUser] = useState({
     email: "",
     password: "",
@@ -10,28 +10,28 @@ export default () => {
 
   const handleChange = ({ target }) => {
     setUser({
-      ...user, [target.name]: target.value,
-    })
+      ...user,
+      [target.name]: target.value,
+    });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       const response = await fetch("/api/user/login", {
         method: "POST",
         headers: {
-          "Accept": "application/json",
+          Accept: "application/json",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(user),
-      })
+      });
 
       const responseJson = await response.json();
       localStorage.setItem("token", responseJson.token);
 
       alert(responseJson.message);
-
     } catch (error) {
       alert("ログイン失敗");
     }
@@ -39,14 +39,32 @@ export default () => {
 
   return (
     <>
-      <Head><title>ログイン</title></Head>
+      <Head>
+        <title>ログイン</title>
+      </Head>
       <h1 className="page-title">ログイン</h1>
       <form onSubmit={handleSubmit}>
-        <input value={user.email} onChange={handleChange} type="text" name="email" placeholder="メールアドレス" required />
-        <input value={user.password} onChange={handleChange} type="text" name="password" placeholder="パスワード" required />
+        <input
+          value={user.email}
+          onChange={handleChange}
+          type="text"
+          name="email"
+          placeholder="メールアドレス"
+          required
+        />
+        <input
+          value={user.password}
+          onChange={handleChange}
+          type="text"
+          name="password"
+          placeholder="パスワード"
+          required
+        />
 
         <button>ログイン</button>
       </form>
     </>
-  )
-}
+  );
+};
+
+export default Login;
--- Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/pages/user/register.tsx
+++ Fullstack_apps_created_with_NextJs_and_TypeScript/next-market/pages/user/register.tsx
@@ -1,8 +1,8 @@
+import type { NextPage } from "next";
 import { useState } from "react";
 import Head from "next/head";
 
-export default () => {
-
+const Register: NextPage = () => {
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -11,42 +11,60 @@ export default () => {
 
   const handleChange = ({ target }) => {
     setUser({
-      ...user, [target.name]: target.value,
-    })
+      ...user,
+      [target.name]: target.value,
+    });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       const response = await fetch("/api/user/create", {
         method: "POST",
         headers: {
-          "Accept": "application/json",
+          Accept: "application/json",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(user),
-      })
+      });
 
       const responseJson = await response.json();
       alert(responseJson.message);
-
     } catch (error) {
       alert("ユーザー登録失敗");
     }
   };
 
   return (
-    <>
-      <Head><title>ユーザー登録</title></Head>
+    <div>
+      <Head>
+        <title>ユーザー登録</title>
+      </Head>
       <h1 className="page-title">ユーザー登録</h1>
       <form onSubmit={handleSubmit}>
         <input value={user.name} onChange={handleChange} type="text" name="name" placeholder="名前" required />
-        <input value={user.email} onChange={handleChange} type="text" name="email" placeholder="メールアドレス" required />
-        <input value={user.password} onChange={handleChange} type="text" name="password" placeholder="パスワード" required />
+        <input
+          value={user.email}
+          onChange={handleChange}
+          type="text"
+          name="email"
+          placeholder="メールアドレス"
+          required
+        />
+        <input
+          value={user.password}
+          onChange={handleChange}
+          type="text"
+          name="password"
+          placeholder="パスワード"
+          required
+        />
 
         <button>登録</button>
       </form>
-    </>
-  )
-}
+    </div>
+  );
+};
+
+export default Register;
