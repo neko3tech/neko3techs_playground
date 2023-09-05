0a0caa61bfbaa419b80c6607ca2ad494b4baffb2
ねこてっく <113709728+neko3tech@users.noreply.github.com>
2023-09-05 19:11:56 +0900

    メタデータ

---
+++ Simple_Blogsite_made_with_Astro_and_TinaCMS/my-blog/src/lib/constants.js
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  siteTitle: "NekoTech Flow.",
+  siteDesc: "NekoTechのブログ",
+  siteUrl: "https://nekomesi.server-on.net",
+  siteType: "website",
+  siteLocale: "ja_JP",
+  siteIcon: "/favicon.png",
+  siteImg: "/ogp.jpg"
+};
